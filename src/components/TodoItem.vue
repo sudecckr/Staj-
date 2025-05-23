f3a65aeddebfcb76b33b013b8fe38960c16605cf
sudecckr <106037397+sudecckr@users.noreply.github.com>
2025-05-24 01:01:45 +0300

    TodoList

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    {{ todo.text }}
+    <button @click="$emit('delete-todo', todo.id)">Sil</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+li {
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 0;
+  border-bottom: 1px solid #eee;
+}
+button {
+  background-color: #ff5c5c;
+  color: white;
+  border: none;
+  padding: 4px 8px;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <TodoItem
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @delete-todo="$emit('delete-todo', todo.id)"
+    />
+  </ul>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+
+export default {
+  props: {
+    todos: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    TodoItem
+  }
+}
+</script>
